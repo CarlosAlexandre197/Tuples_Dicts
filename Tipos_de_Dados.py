6568c188582c9c6dc9c6397c753d24460f62ae1e
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2023-04-09 15:13:15 -0300

    Update Tipos_de_Dados.py

--- Tipos_de_Dados.py
+++ Tipos_de_Dados.py
@@ -1,10 +1,10 @@
 '''Uma característica em comum dentre os tuples, lists e dicts é permitir qualquer tipo de dado. 
 Os tipos de dados existentes na linguagem Python podem ser divididos em categorias:'''
 
- Texto:	 str
- Numéricos:	 int, float, complex
- Sequências:	 list, tuple, range
- Mapeamento:	 dict
- Conjuntos:	 set, frozenset
- Boolean:	 bool
- Binários:	 bytes, bytearray, memoryview
+ texto =	 str
+ numericos = int, float, complex
+ sequencias =	 list, tuple, range
+ mapeamento =	 dict
+ conjuntos =	 set, frozenset
+ boolean =	 bool
+ binarios =	 bytes, bytearray, memoryview
